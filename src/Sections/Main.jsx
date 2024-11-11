1fcafea76fce02c5a95c7147e0de1b62aec2c8cf
Kai Mitchell <kaimitchell.personal@gmail.com>
2024-11-11 19:24:11 +1300

    implement infinite scrolling using random word api

--- src/Sections/Main.jsx
+++ src/Sections/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 function Main() {
+    const [itemCount, setItemCount] = useState(12);
     const [movies, setMovies] = useState([
         'The Starling', 
         'Bright Star', 
@@ -8,36 +9,37 @@ function Main() {
         'Star Trek'
     ]);
 
-    //Use to prevent api calls on every rerender
-    // useEffect(() => {
-    //     generateWords();
-    // }, []);
-
-    //Laptop is getting hot
-    // async function generateWords() {
-    //     const response = await fetch('https://random-word-api.herokuapp.com/all');
-    //     const data = await response.json();
-
-    //     setMovies(prev => [
-    //         ...prev,
-    //         ...data.slice(0, 100)
-    //     ]);
-        
-    // };
-
-    function generateWords() {
-        const moviePlaceholders = [];
-        for(let i = 0; i < 100; i++) {
-            moviePlaceholders.push(i);
+    useEffect(() => {
+        async function generateWords() {
+            const response = await fetch(`https://random-word-api.herokuapp.com/word?number=${itemCount}`);
+            const data = await response.json();
+    
+            setMovies(prev => [
+                ...prev,
+                ...data
+            ]);
+    
+            console.log(data);
         };
 
-        setMovies(prev => [
-            ...prev,
-            ...moviePlaceholders
-        ]);
-    };
+        generateWords();
+    }, [itemCount]);
 
-    generateWords();
+    function handleScroll() {
+        console.log('HEIGHT: ', document.documentElement.scrollHeight);
+        console.log('TOP: ', document.documentElement.scrollTop);
+        console.log('WINDOW: ', window.innerHeight);
+
+        // + 1 sum to account for some browsers inner height and scroll top values not equalling scroll heights value
+        if(window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
+            setItemCount(prev => prev + 1);
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+    }, []);
+
 
     return(
         <main className='w-5/6'>
