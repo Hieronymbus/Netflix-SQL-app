b15d109006a6c973ec401c3a0e9faf875f353951
Kai Mitchell <kaimitchell@gmail.com>
2024-11-12 15:34:42 +1300

    Create states for filter buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Sections/Header.jsx';
 import Main from './Sections/Main.jsx';
 
 export default function App() {
+  const [isDuration, setDuration] = useState(false);
+  const [isReleaseYear, setRelaseYear] = useState(false);
+  const [isRating, setRating] = useState(false);
+
   return (
     <>
       <div className='w-full p-2.5 mx-auto bg-gray-200 flex flex-col gap-10'>
-        <Header />
-        <Main />
+        <Header 
+          setDuration={setDuration}
+          isDuration={isDuration}
+          setRelaseYear={setRelaseYear}
+          isReleaseYear={isReleaseYear}
+          setRating={setRating}
+          isRating={isRating}
+        />
+        <Main 
+          setDuration={setDuration}
+          isDuration={isDuration}
+          setRelaseYear={setRelaseYear}
+          isReleaseYear={isReleaseYear}
+          setRating={setRating}
+          isRating={isRating}
+        />
       </div>
     </>
   )
