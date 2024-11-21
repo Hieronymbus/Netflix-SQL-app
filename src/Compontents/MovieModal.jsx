91218f37ab5dae6dbbce4a925b20f0ce883e0c47
Hieronymbus <birdsallalexander1990@gmail.comm>
2024-11-21 13:17:07 +1300

    updated loading details text to be bigger and also include the close button for the modal in top right corner while loading

--- src/Compontents/MovieModal.jsx
+++ src/Compontents/MovieModal.jsx
@@ -67,7 +67,25 @@ const MovieModal = ( { setIsModalFor, isModalFor } ) => {
       {
          isLoadingDetails 
          ?
-         <p>loading</p> 
+         <div className="flex justify-between">
+               <div className="pl-5 pt-2 ">
+                  <h2 
+                     className="text-2xl"
+                  >
+                     LOADING details..
+                  </h2>
+               </div>
+               <div>
+                  <button 
+                     className="bg-slate-950 rounded p-1 text-2xl border border-black "
+                     onClick={ () => setIsModalFor("") }
+                  > 
+                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                     </svg>
+                  </button>
+               </div>
+            </div>
          :
          <div>
             <div className="flex justify-between">
